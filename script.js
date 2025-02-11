e39e8c1319d423180dea8790d2c8b0b5d2bcdc97
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-11 11:39:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let student = {
 	name: 'John',
 };
-
-function getKeys(obj) {
-	return Object.keys(obj);
+Object.prototype.getKeys=function 
+	{
+	return Object.keys(this);
 }
 
 console.log(getKeys(student));

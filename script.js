8f7310acfa47c9fe6e7d957304a7b146ca3312e7
harshps900 <151460810+harshps900@users.noreply.github.com>
2025-02-11 11:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-//your JS code here. If required.
-let student={
-	name:'John',
+let student = {
+	name: 'John',
 };
+
 function getKeys(obj) {
 	return Object.keys(obj);
 }
+
 console.log(getKeys(student));
